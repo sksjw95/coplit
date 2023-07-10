d3dc38f507cc7c172e33c199cc54cc7cc412386d
sksjw95 <tjwldnd95@hanmail.net>
2023-07-10 22:11:17 +0900

    coplit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3));
+
+        System.out.println(list);
+    }
+}
